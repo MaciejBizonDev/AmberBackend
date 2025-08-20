2c655f427ed176b9e8319a227826453655c12cfb
MaciejBizonDev <130704701+MaciejBizonDev@users.noreply.github.com>
2025-08-21 01:39:16 +0200

    update

---
+++ AmberBackend/Controllers/DoubleValueController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourNamespace.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DoubleValueController : ControllerBase
+    {
+        // POST api/DoubleValue
+        [HttpPost]
+        public ActionResult<int> Post([FromBody] int value)
+        {
+            int doubled = value * 2;
+            return Ok(doubled);
+        }
+    }
+}
